05cc55be38931ff2925f02bd0d8267c219181873
kkokwara2014 <kkokwara2014@gmail.com>
2019-12-12 11:50:12 +0100

    --- Auto-Git Commit ---

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use PDF;
+use App\Childreg;
+
 class PdfController extends Controller
 {
-    public function print(){
-        
+    public function print($id){
+        $birthreg=Childreg::find($id);
+        $pdf=PDF::loadView('admin.birthreg.cert',['birthreg'=>$birthreg])->setPaper('a4','portrait');
+        $fileName=$birthreg->certnumber;
+
+        return $pdf->stream($fileName);
     }
 }
--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
          * Package Service Providers...
          */
 
-         Barryvdh\DomPDF\ServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'PDF'=>Barryvdh\DomPDF\Facade::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
