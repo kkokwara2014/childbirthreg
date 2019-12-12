52081eedaeca77a2febe0735e185f9122072cbba
kkokwara2014 <kkokwara2014@gmail.com>
2019-12-12 02:24:48 +0100

    --- Auto-Git Commit ---

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->string('othername');
             $table->string('email')->unique();
+            $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('userimage')->default('defaultuserimage.jpg');
             $table->rememberToken();
             $table->timestamps();
         });
